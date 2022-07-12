66e966b26ceacedae5e1c8e911a78b66d03515a5
Beloglazov Alexey <barber004@mail.ru>
2022-07-12 10:53:12 +0300

    2_2

---
+++ lesson2/2_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int MaxDigit(int num)
+{
+    if (num / 10 > num % 10)
+        return num / 10;
+    else
+        return num % 10;
+}
+int a = new Random().Next(10,100);
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a));
