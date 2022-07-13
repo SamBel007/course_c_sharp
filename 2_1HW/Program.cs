ce19b6aa08e01c07d4ac4d38b4b8c5b2ffdad8b5
Beloglazov Alexey <barber004@mail.ru>
2022-07-13 12:31:52 +0300

    внесли изминения

--- 2_1HW/Program.cs
+++ 2_1HW/Program.cs
@@ -9,3 +9,12 @@ else
 {
     Console.WriteLine(n % 100 / 10);
 }
+
+
+//Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+//    while(true)
+//    {
+//        string input = Console.ReadLine();
+//        if(!input.Equals("exit"))
+//            Console.WriteLine("{0}->{1}",input, input[1]);
+//        else
