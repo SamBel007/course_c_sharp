ab613204d695f4d5079b0245a641488450523097
Алексей Белоглазов <108532822+SamBel007@users.noreply.github.com>
2022-07-13 17:18:17 +0300

    Delete 2_1homework directory

--- 2_1homework/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Write number n: ");
-    while(true)
-    {
-        string input = Console.ReadLine();
-        if(!input.Equals("exit"))
-            Console.WriteLine("{0}->{1}",input, input[1]);
-    }
