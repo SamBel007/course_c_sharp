6e552138ab8e733e8d0286e3f724671fc4fe7a66
Beloglazov Alexey <barber004@mail.ru>
2022-07-19 15:00:57 +0300

    test3

---
+++ 3_3HW/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter N: ");
+int a=int.Parse(Console.ReadLine());
+int Cube(int a)
+{
+    int i=1;
+    for(i=1;i<=a;i++)
+        Console.Write($"{Math.Pow(i,3)} ");
+    return(0);
+}
+Cube(a);
